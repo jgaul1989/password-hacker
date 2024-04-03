fa9063ed0c64990502f1312ada7bd85d579630fd
Jonathan <jonathan.gaul1@gmail.com>
2024-04-03 17:17:16 -0400

    Refactored code to add functions for sending a message to a host address

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import socket
 import argparse
 
+
+BUFFER_SIZE = 1024
+
+
+def create_socket():
+    return socket.socket()
+
+def send_message(client_socket, address, message):
+    client_socket.connect(address)
+    client_socket.send(message.encode())
+    return client_socket.recv(BUFFER_SIZE).decode()
+
+
+def main(ip_address, port, message):
+    address = (ip_address, port)
+    with create_socket() as client_socket:
+        response = send_message(client_socket, address, message)
+        print(response)
+
+
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser()
     parser.add_argument("ip_address", type=str)
     parser.add_argument("port", type=int)
     parser.add_argument("message", type=str)
     args = parser.parse_args()
 
-    with socket.socket() as client_socket:
-        host_name = args.ip_address
-        port = args.port
-        address = (host_name, port)
-        client_socket.connect(address)
-        data = args.message.encode()
-        client_socket.send(data)
-        response = client_socket.recv(1024)
-        response_str = response.decode()
-        print(response_str)
+    # Basic validation can be added here
+    main(args.ip_address, args.port, args.message)
